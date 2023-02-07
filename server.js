cbfb24e7e47f6c25f8f28d515e1de836a27313d0
Big Trees <traveye@protonmail.com>
2023-02-07 00:25:48 -0800

    adding session comment, saving work

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(session({
-  secret: 'secret',
+  secret: 'secret', //a random string, should not be exposed, usally long and randomly generated
   resave: false,
   saveUninitialized: false,
   store: sessionStore,
@@ -22,3 +22,4 @@ sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
 
+//"express-session" is a session management middleware, while "connect-session-sequelize" is a specific implementation of a session store that uses the Sequelize ORM to store session data in a database. "express-session" provides the general mechanism for managing user sessions, while "connect-session-sequelize" provides a way to store the session data in a persistent manner.
