7b7228af54287045a79eba3d50006e041763e936
Big Trees <traveye@protonmail.com>
2023-02-08 21:27:46 -0800

    adding nodemon script, updating server and connection

--- config/connection.js
+++ config/connection.js
@@ -1,24 +1,22 @@
-const Sequelize = require("sequelize");
-const session = require("express-session");
-const SequelizeStore = require("connect-session-sequelize")(session.Store);
-require("dotenv").config('/.env');
+const Sequelize = require('sequelize');
+require('dotenv').config();
 
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(
-      process.env.DB_NAME,
-      process.env.DB_USER,
-      process.env.DB_PASSWORD,
-      {
-        host: "localhost",
-        dialect: "mysql",
-        port: 3306,
-        dialectOptions: {
-          decimalNumbers: true,
-        },
-      }
-    );
+let sequelize;
 
-const sessionStore = new SequelizeStore({ db: sequelize });
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306
+    }
+  );
+}
 
-module.exports = { sequelize, sessionStore };
+module.exports = sequelize;
+
--- server.js
+++ server.js
@@ -1,45 +1,39 @@
 const express = require('express');
 const session = require('express-session');
-const exphbs = require('express-handlebars');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
-
 const routes = require('./controllers');
-const { sequelize, sessionStore } = require('./config/connection');
+const path = require('path');
+const exphbs = require('express-handlebars');
+
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+// const helpers = require('./utils/helpers');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.use(session({
-  secret: 'toseeaworldinagrainofsand',
-  cookie: {
-    maxAge: 3600,
-    httpOnly: true,
-    secure: false,
-    sameSite: 'strict',
-  },
-  resave: false,
-  saveUninitialized: false,
-  store: new SequelizeStore({
-    db: sequelize,
-  }),
-}));
-
-const hbs = exphbs.create({});
+const hbs = exphbs.create();
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log(`\nServer running on port ${PORT}. Visit http://localhost:${PORT} and create an account!`));
+  app.listen(PORT, () => console.log(`Now listening on ${PORT}`));
 });
-
-//"express-session" is a session management middleware, while "connect-session-sequelize" is a specific implementation of a session store that uses the Sequelize ORM to store session data in a database. "express-session" provides the general mechanism for managing user sessions, while "connect-session-sequelize" provides a way to store the session data in a persistent manner.
